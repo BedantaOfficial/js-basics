d548c7b84fe830c2a9c0d952a9c03327428385dc
BedantaOfficial <168219814+BedantaOfficial@users.noreply.github.com>
2024-07-08 11:22:49 +0000

    01_conversionoperation

---
+++ 01_basics/01_conversionoperation.js
@@ -0,0 +1,49 @@
+let score = "48abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//note
+// 33 => 33
+// 33anc => NaN (not a number) but type number
+// true => 1, false=> 0
+
+
+//let isLoggedIn = 1
+
+//let isLoggedIn = ""
+
+let isLoggedIn = "Bedanta"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanisLoggedIn);
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+//console.log(stringNumber)
+//console.log(typeof stringNumber);
+
+
+
+
+// operations
+
+let value = 3
+let negValue = (-value)
+
+console.log(negValue);
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
