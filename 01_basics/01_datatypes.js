db8d09def4a585d3402f0796ba7f39217ba94e1a
BedantaOfficial <168219814+BedantaOfficial@users.noreply.github.com>
2024-07-08 07:13:19 +0000

    variables and datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,8 @@
+"use strict";  // treat all the code as newer version
+
+const name = "Bedanta"
+let age = 23
+let profession = "Web developer"
+
+console.log(typeof null);
+console.log(typeof age);
---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 123434
+let accountEmail = "bed@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+accountEmail = "bedanta@gmail"
+accountPassword = "67891"
+accountCity = "Guwahati"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
