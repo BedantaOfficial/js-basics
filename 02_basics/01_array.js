a11752ee77d6750ae29990bcd864308f0a51713e
BedantaOfficial <168219814+BedantaOfficial@users.noreply.github.com>
2024-07-10 05:34:18 +0000

    array basics in js

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -3,7 +3,32 @@ const myHeros = ["Shaktimaan", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
 
-console.log(myArr[4])
+// console.log(myArr[4])
 
-myArr.push(6)
-console.log(myArr)
+// myArr.push(6)
+// myArr.push(9)
+// myArr.pop()
+// myArr.unshift(10)
+// myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(9))
+// console.log(myArr.indexOf(3))
+
+
+// const newArr = myArr.join()
+// console.log(myArr)
+// console.log(newArr)
+
+// slice and splice in array
+
+console.log("A ", myArr);
+
+const myN1 = myArr.slice(1, 3)
+console.log(myN1);
+
+console.log("B ", myArr);
+
+const myN2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myN2)
