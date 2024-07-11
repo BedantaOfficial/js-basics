5d95404caca042fdaceea1a8d4d4a32b162899b5
BedantaOfficial <168219814+BedantaOfficial@users.noreply.github.com>
2024-07-11 06:20:50 +0000

    function updated

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -4,8 +4,65 @@ function sayMyName(){
 
 // sayMyName()
 
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2)
+// }
+
 function addTwoNumbers(number1, number2){
-    console.log(number1 + number2)
+   
+    // let result = number1 + number2
+    // return result
+
+    return number1 + number2
 }
 
-addTwoNumbers(3, 9)
+const result = addTwoNumbers(3, 9)
+
+console.log("Result: ", result);
+
+
+function loginUserMessage(username = "chiman"){
+    if(!username){
+       console.log("Please enter a username")
+       return
+    }
+    return `${username} you just logged In`
+}
+
+console.log(loginUserMessage("Bedanta"))
+
+
+
+function calculateCartPrice(val1, val2, ...num1){    
+    return num1
+}
+
+console.log(calculateCartPrice(200, 300, 400))
+
+
+const obj = {
+    username: "Bedanta",
+    price: 300
+}
+
+function handleObject(anyobject){
+      console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(obj)
+
+handleObject({
+    username: "Abhishek",
+    price: 600
+})
+
+
+const myNewArray = [100, 200, 300, 400]
+
+function returnSecondValue(getarray){
+    return getarray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+
+console.log(returnSecondValue([100, 300, 400, 500]))
